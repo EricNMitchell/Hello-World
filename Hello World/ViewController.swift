e363d91fa194a416c2f8ba3782fe528f81db66bb
Eric <emitchell008@gmail.com>
2019-01-28 20:08:44 -0600

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Eric Mitchell on 1/28/19.
+//  Copyright © 2019 Eric Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
